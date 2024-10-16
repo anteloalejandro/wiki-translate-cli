66d1a74344147f45fa6422ffbf61fe0fc59296e5
Alejandro Antelo <alfasant@protonmail.com>
2024-10-16 12:16:37 +0200

    migrate to inquirer and add basic error handling and colors

--- index.js
+++ index.js
@@ -1,57 +1,83 @@
-import enquirer from "enquirer";
-import { search, getTranslation } from "wiki-translator";
+import { search, confirm } from "@inquirer/prompts";
+import { search as wikiSearch, getTranslation } from "wiki-translator";
 import { languages } from "wiki-translator/languages.js";
+import fuzzysearch from 'fuzzysearch';
+import colors from 'yoctocolors-cjs';
 
-const langKeys = [...languages.keys()];
-const response = await enquirer.prompt([
-  {
-    type: "autocomplete",
-    name: "sourceLang",
-    message: "Source Language: ",
-    choices: langKeys,
-    limit: 5,
-  },
-  {
-    type: "autocomplete",
-    name: "targetLang",
+async function sleep(ms) {
+  await new Promise(resolve => setTimeout(resolve, ms));
+  return;
+}
+
+async function onerror(error) {
+  if (['AbortPromptError', 'ExitPromptError'].includes(error.name)) {
+    return;
+  }
+
+  throw error;
+}
+
+async function run() {
+  const langObj = Array.from(languages).map(([a, b]) => ({ name: a, value: b }));
+  function fuzzyLang(input) {
+    return langObj.filter(o => fuzzysearch(input.toLowerCase(), o.name.toLowerCase()));
+  }
+
+  const sourceLang = await search(
+    {
+      message: "Source Language: ",
+      source: (input) => {
+        if (!input) return langObj;
+        return fuzzyLang(input);
+      }
+    },
+  ).catch(onerror);
+  if (!sourceLang) {
+    return;
+  }
+
+  const targetLang = await search({
     message: "Target Language: ",
-    choices: langKeys,
-    limit: 5,
-  },
-  {
-    type: "input",
-    name: "term",
-    message: "Search for a term: ",
+    source: (input) => {
+      if (!input) return langObj;
+      return fuzzyLang(input);
+    }
+  }).catch(onerror);
+  if (!targetLang) {
+    return;
   }
-]);
-const pages = await search(response.term, languages.get(response.sourceLang));
 
-let translation = null;
-let confirmation = true
-do {
-  const select = new enquirer.Select({
-    name: "id",
-    message: "Choose one!",
-    choices: pages.map(p => ({
-      name: p.pageid,
-      message: p.title
-    }))
-  });
-  const id = await select.run();
 
-  translation = await getTranslation(
-    id,
-    languages.get(response.sourceLang),
-    languages.get(response.targetLang)
+  let translation = null;
+  let confirmation = true
+  do {
+    const id = await search({
+      message: "Type a term!",
+      source: async (input, { signal }) => {
+        await sleep(300);
+        if (signal.aborted || !input || input.length < 3) return [];
+
+        const pages = await wikiSearch(input, sourceLang);
+        return pages.map(o => ({
+          value: o.pageid,
+          name: o.title,
+          description: o.snippet
+        }));
+      }
+    }).catch(onerror);
+
+    translation = await getTranslation(id, sourceLang, targetLang);
+    if (!translation) {
+      confirmation = await confirm({
+        message: colors.red("Could not find term. Try again?")
+      })
+    }
+  } while (!translation && confirmation);
+
+  console.log(
+    colors.green(colors.bold("Translation: ")) + colors.green(translation.title) + '\n'
+      + colors.green(colors.bold("URL: ")) + colors.blue(colors.underline(translation.url))
   );
+}
 
-  if (!translation) {
-    const confirm = new enquirer.Confirm({
-      name: "confirmation",
-      message: "Could not find translation. Try again?"
-    })
-    confirmation = await confirm.run();
-  } else {
-    console.log(translation.title);
-  }
-} while (!translation && confirmation);
+run();
